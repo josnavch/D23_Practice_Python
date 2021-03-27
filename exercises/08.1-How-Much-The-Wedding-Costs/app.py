8ee85003a8662fbd1e62369c59e450383f971068
josnavch <josnavch@gmail.com>
2021-03-27 03:03:20 +0000

    Exercises 14 JN

--- exercises/08.1-How-Much-The-Wedding-Costs/app.py
+++ exercises/08.1-How-Much-The-Wedding-Costs/app.py
@@ -1,6 +1,16 @@
 user_input = int(input('How many people are coming to your wedding?\n'))
 
-if user_input <= 50:
+
+if user_input > 200:
+    price = 20000
+elif user_input == 200:
+    price = 15000
+elif user_input > 100:
+    price = 15000
+elif user_input > 50:
+    price = 10000
+elif user_input <= 50:
     price = 4000
+
 
 print('Your wedding will cost '+str(price)+' dollars')
--- exercises/11-Create-A-New-Function/app.py
+++ exercises/11-Create-A-New-Function/app.py
@@ -1,3 +1,12 @@
 import random
 
 # your code here
+
+def generate_random ():
+    number = random.randint(0, 9)
+    return  number
+
+
+print(generate_random())
+
+
